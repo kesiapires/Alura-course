7bdeb710e1465a55c97412a2704d8f771ee1fd23
Kesia Pires <kesiapiressantos@gmail.com>
2024-07-13 23:34:04 +0100

    Documentation - DocString.

---
+++ python/docstrings.py
@@ -0,0 +1,45 @@
+#Crie uma docstring para a função exibir_nome_do_programa()
+'''Exibe o nome estilizado do programa na tela'''
+
+#Crie uma docstring para a função exibir_opcoes()
+'''Exibe as opções disponíveis no menu principal'''
+
+#Crie uma docstring para a função finalizar_app()
+'''Exibe uma mensagem de finalização do aplicativo'''
+
+#Crie uma docstring para a função opcao_invalida()
+'''Exibe uma mensagem de erro caso digitado um valor inválido.
+Outputs: 
+-Retorna ao menu principal'''
+
+#Crie uma docstring para a função exibir_subtitulo(texto)
+'''Exibir subtítulo estilizado na tela
+Inputs:
+-texto: str - o texto do subtítulo '''
+
+#Crie uma docstring para a função cadastrar_novo_restaurante()
+'''Essa função cadastra retaurantes 
+Inputs:
+-Nome do restaurante
+-Categoria
+
+Outputs:
+-Cadastra novos restaurantes a lista de restaurantes'''
+
+#Crie uma docstring para a função listar_restaurantes()
+'''Exibe uma lista de restaurantes cadastrados
+Outputs:
+-Exibe a lista na tela'''
+
+#Crie uma docstring para a função alternar_estado_restaurante()
+'''Essa função alterna o estado do restaurante, podendo ser ativo ou inativo.
+Outputs: 
+-Exibe mensagem de sucesso da alteração'''
+
+#Crie uma docstring para a função escolher_opcao()
+'''Solicita e executa a opção escolhida pelo usuário.
+Output: 
+-Executa a opção escolhida pelo usuário'''
+
+#Crie uma docstring para a função main()
+'''Função principal que inicia o programa'''
