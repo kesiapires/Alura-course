c9e9c0ed74b532b6b2ee1677ad89b452821d7ccd
Kesia Pires <kesiapiressantos@gmail.com>
2024-07-16 13:05:18 +0100

    extension exercise 010

---
+++ python/oop_exercise010extensao.py
@@ -0,0 +1,16 @@
+from oop_exercise010 import Restaurante
+
+restaurante_praca = Restaurante('Praça','Gourmet') 
+restaurante_praca.alterar_estado()
+restaurante_pizza = Restaurante('Domino`s pizza', 'Fast Food')
+
+Restaurante.listar_restaurantes()
+print()
+
+
+
+def main():
+    pass
+
+if __name_ == '__main__':
+    main()
