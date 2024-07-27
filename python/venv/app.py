21701c0e77561a83fb9727e65db27378d166e5c3
Kesia Pires <kesiapiressantos@gmail.com>
2024-07-27 02:12:50 +0100

    Filtering data

--- python/venv/app.py
+++ python/venv/app.py
@@ -9,9 +9,20 @@ response = requests.get(url)
 
 #200 é que deu certo
 #404 erro
+print(response)
 if response.status_code == 200:
     dados_json = response.json()
-    print(dados_json)
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })    
 else:
     #não funciona
     print(f'O erro foi: {response.status_code}')
+print(dados_restaurante['McDonald’s'])
